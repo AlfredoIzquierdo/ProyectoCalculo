e719e085b1ac81d062d3ebc21bce86ba3d6cac9a
Christian Duarte <w.christian.duarte.@outlook.com>
2020-11-27 01:16:50 -0700

    agregadas las funciones uwu

--- CalculoProtecto/CalculoProtecto/Form3.cs
+++ CalculoProtecto/CalculoProtecto/Form3.cs
@@ -22,5 +22,13 @@ namespace CalculoProtecto
         {
 
         }
+
+        public string funcionVolumen(double ancho, double alto) {
+            return "4x^3-" + ((ancho + alto) * 2) + "x^2+" + (ancho*alto)+"x"; 
+        }
+
+        public string derivadaVolumen(double ancho, double alto) {
+            return "12x^2-" + ((ancho + alto) * 4) + "x+" + (ancho * alto);
+        }
     }
 }
